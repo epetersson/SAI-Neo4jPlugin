375fda72e0d5146950963f0cffe12583fefa64d7
grasshopper <emil.petersson@live.com>
2017-12-03 14:23:45 -0600

    Graph now imlements sai-graph

--- src/main/java/net/sourcedestination/sai/neo4jplugin/domain/Graph.java
+++ src/main/java/net/sourcedestination/sai/neo4jplugin/domain/Graph.java
@@ -1,16 +1,18 @@
 package net.sourcedestination.sai.neo4jplugin.domain;
 
+import com.google.common.collect.Sets;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * Created by ep on 2017-11-27.
  */
 @NodeEntity
-public class Graph extends Entity {
+public class Graph extends Entity implements net.sourcedestination.sai.graph.Graph {
     public Graph(){
 
     }
@@ -33,7 +35,37 @@ public class Graph extends Entity {
         return this.nodes;
     }
 
-    public Set<Feature> getFeatures() {
-        return this.features;
+    @Override
+    public Stream<Integer> getEdgeIDs() {
+        return null;
+    }
+
+    @Override
+    public Stream<Integer> getNodeIDs() {
+        return null;
+    }
+
+    public Stream<net.sourcedestination.sai.graph.Feature> getFeatures() {
+        return (Stream<net.sourcedestination.sai.graph.Feature>) this.features;
+    }
+
+    @Override
+    public Stream<net.sourcedestination.sai.graph.Feature> getNodeFeatures(int i) {
+        return null;
+    }
+
+    @Override
+    public Stream<net.sourcedestination.sai.graph.Feature> getEdgeFeatures(int i) {
+        return null;
+    }
+
+    @Override
+    public int getEdgeSourceNodeID(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getEdgeTargetNodeID(int i) {
+        return 0;
     }
 }
